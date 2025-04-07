43f4dbb96c9ebfbc03f7833324469c866e78625f
unknown <sopesquisa133@gmail.com>
2025-04-07 13:57:55 -0300

    Exercío 1

---
+++ Exercício 1/index.js
@@ -0,0 +1,11 @@
+let numero = parseFloat(prompt("Insira um número")) // Declarar a váriavel
+
+if (numero > 0) { //Condição com maior que zero
+    alert("O número é positivo")
+}
+if (numero == 0) { //Condição se for igual a zero
+    alert("O número é neutro")
+}
+if (numero < 0) { //Condição se for menor que zero
+    alert("O número não é positivo")
+}
