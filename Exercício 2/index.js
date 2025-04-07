a8df9b277131214f416a1ef282afe351fdf6c03c
unknown <sopesquisa133@gmail.com>
2025-04-07 14:45:44 -0300

    Exercício 3

--- Exercício 2/index.js
+++ Exercício 2/index.js
@@ -1,12 +1,12 @@
-let idade = parseInt(prompt("Insira sua idade"))
+let idade = parseInt(prompt("Insira sua idade")); //Declarar  a váriavel da idade
 
-while (isNaN(numero)){
-    idade = parseFloat(prompt("Insira um valor válido")) //enquanto o usuário não inserir um valor válido ele poderá tentar novamente
+while (isNaN(idade)){ //enquanto o usuário não inserir um valor válido ele poderá tentar novamente
+    idade = parseFloat(prompt("Insira um valor válido")); 
 }
 
-if (idade >= 18){
-    alert("você é maior de idade")
+if (idade >= 18){ //Condição maior de idade e mostra de resultados
+    alert("você é maior de idade");
 }
-if (idade < 18) {
-    alert("você é menor de idade")
+if (idade < 18) { //Condição menor de idade e mostra de resultados
+    alert("você é menor de idade");
 }
