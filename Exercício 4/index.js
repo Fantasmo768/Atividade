b85212824cfc87243ca7515ad045a24c574aba6c
unknown <sopesquisa133@gmail.com>
2025-04-07 15:20:06 -0300

    Exercício 5

--- Exercício 4/index.js
+++ Exercício 4/index.js
@@ -1,15 +1,15 @@
-let valorCompra = parseFloat(prompt("Insira o valor da sua compra"))
-let valorDesconto = parseFloat(valorCompra*0.1)
-let valorDescontado = parseFloat(valorCompra - valorDesconto)
+let valorCompra = parseFloat(prompt("Insira o valor da sua compra")) //variável que vai pegar o valor da compra
+let valorDesconto = parseFloat(valorCompra*0.1) //variável que define o valor do desconto
+let valorDescontado = parseFloat(valorCompra - valorDesconto) //variável que define o valor total
 
-if (isNaN(valorCompra)){
-    alert ("O valor não é um número") //enquanto o usuário não inserir um valor válido ele poderá tentar novamente
+if (isNaN(valorCompra)){ //condição se o valor não for um número
+    alert ("O valor não é um número") 
 }
 
-if (valorCompra >= 100){
+if (valorCompra >= 100){ //condição se o número for maior ou igual a 100
     alert (`Você ganhou um desconto de 10% por que sua conta passou de 100 reais, o valor do desconto é ${valorDesconto.toFixed(2)} e o valor final é ${valorDescontado.toFixed(2)}`)
 }
 
-if (valorCompra <100) {
+if (valorCompra <100) { //condição se o número for menor que 100
     alert ("Hoje não tem desconto");
 }
---
+++ Exercício 5/index.js
@@ -0,0 +1,13 @@
+let numero = parseInt(prompt("Insira um número")); // Declarar a váriavel do número
+
+while (isNaN(numero)){
+    numero = parseFloat(prompt("Insira um valor válido")) //enquanto o usuário não inserir um valor válido ele poderá tentar novamente
+}
+
+if (numero % 5 == 0) { //Condição se o número for múltiplo de 5 e mostra de resultados
+    alert("Seu número é múltiplo de 5")
+}
+
+if (numero % 5 != 0) { //Condição se o número não for múltiplo de 5 e mostra de resultados
+    alert("Seu número não é múltiplo de 5")
+}
