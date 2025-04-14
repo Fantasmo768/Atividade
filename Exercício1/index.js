2319377a34ba9f73cffae4a63211aa11a8c7a6ef
unknown <sopesquisa133@gmail.com>
2025-04-14 07:56:39 -0300

    Versão final

--- Exercício1/index.js
+++ Exercício1/index.js
@@ -1,6 +1,6 @@
 let numero = parseFloat(prompt("Insira um número")) // Declarar a váriavel
 
-while (isNaN(numero)){
+if (numero != 8 && numero!= 10 && numero!= 9){
     numero = parseFloat(prompt("Insira um valor válido")) //enquanto o usuário não inserir um valor válido ele poderá tentar novamente
 }
 if (numero > 0) { //Condição com maior que zero e mostra de resultados
--- Exercício4/index.js
+++ Exercício4/index.js
@@ -1,10 +1,12 @@
 let valorCompra = parseFloat(prompt("Insira o valor da sua compra")) //variável que vai pegar o valor da compra
+
+
+if (isNaN(valorCompra)){
+    valorCompra = parseFloat(prompt("Insira um valor válido")) //enquanto o usuário não inserir um valor válido ele poderá tentar novamente
+}
+
 let valorDesconto = parseFloat(valorCompra*0.1) //variável que define o valor do desconto
 let valorDescontado = parseFloat(valorCompra - valorDesconto) //variável que define o valor total
-
-if (isNaN(valorCompra)){ //condição se o valor não for um número
-    alert ("O valor não é um número") 
-}
 
 if (valorCompra >= 100){ //condição se o número for maior ou igual a 100
     alert (`Você ganhou um desconto de 10% por que sua compra passou de 100 reais, o valor do desconto é ${valorDesconto.toFixed(2)} e o valor final é ${valorDescontado.toFixed(2)}`)
